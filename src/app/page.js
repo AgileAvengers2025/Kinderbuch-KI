20f29f668170b3d442c7aa1116d991d153015d74
mateusmorgado <mateusmorgado@hotmail.com>
2025-01-29 00:52:13 +0100

    testing branching protection

--- src/app/page.js
+++ src/app/page.js
@@ -21,9 +21,13 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-                   <li>Start doing the <strong>Kinderbuch-KI</strong> project</li>
+          <li>
+            Start doing the <strong>Kinderbuch-KI</strong> project
+          </li>
         </ol>
-
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">testing branch protection</li>
+        </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
