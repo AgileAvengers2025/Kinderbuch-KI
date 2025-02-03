aec53a39f312444201e4c567322906264392300f
jpg344 <174099082+jpg344@users.noreply.github.com>
2025-02-03 17:36:33 +0100

    added schema to the models for story and user

--- Backend/models/Story.js
+++ Backend/models/Story.js
@@ -1,7 +1,57 @@
 const mongoose = require('mongoose');
 
-const StorySchema = new mongoose.Schema({
-//schema still needs to be filled
+const StorySchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+      ref: 'User',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: [
+      {
+        id: {
+          type: Number,
+          required: true,
+        },
+        text: {
+          type: String,
+          required: true,
+        },
+        image: {
+          type: String,
+          default: '',
+        },
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Custom id will set to the same as MongoDB's _id
+StorySchema.pre('save', function (next) {
+  if (!this.id) {
+    this.id = this._id.toString();
+  }
+  next();
 });
 
-module.exports = Story = mongoose.model('story', StorySchema);
+module.exports = mongoose.model('Story', StorySchema);
--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,7 +1,71 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-//schema still needs to be filled
+const UserSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address'],
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    displayName: {
+        type: String,
+        default: '',
+      },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    name: {
+        type: String,
+        default: '',
+      },
+    address: {
+      street: {
+        type: String,
+      },
+      postalCode: {
+        type: String,
+        match: [/^\d{5}$/, 'Bitte eine gültige Postleitzahl eingeben'],
+      },
+      city: {
+        type: String,
+      },
+      country: {
+        type: String,
+        default: 'Deutschland',
+      },
+    },
+    kidsNames: {
+        type: [String],
+        default: [],
+      },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Custom id will set to the same as MongoDB's _id
+UserSchema.pre('save', function (next) {
+  if (!this.id) {
+    // If there is no other id set
+    this.id = this._id.toString();
+  }
+  next();
 });
 
-module.exports = User = mongoose.model('user', UserSchema);
+module.exports = mongoose.model('User', UserSchema);
