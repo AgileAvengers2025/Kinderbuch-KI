92b3396dc01aac784f06c2d9a6c5465b43d35854
jpg344 <174099082+jpg344@users.noreply.github.com>
2025-02-03 09:59:08 +0100

    basic structure backend created

---
+++ Backend/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/api/users");
+const storyRoutes = require("./routes/api/stories");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// use the cors middleware with the
+// origin and credentials options
+app.use(cors({ origin: true, credentials: true }));
+
+// use the body-parser middleware to parse JSON and URL-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// use the routes module as a middleware
+// for the /api/books path
+app.use("/api/users", userRoutes);
+app.use("/api/stories", storyRoutes);
+
+// Connect Database
+connectDB();
+
+app.get("/", (req, res) => res.send("Hello world!"));
+const port = process.env.PORT || 8082;
+app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ Backend/routes/api/stories.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+// Load Story model
+const Story = require('../../models/Story');
+
+module.exports = router;
